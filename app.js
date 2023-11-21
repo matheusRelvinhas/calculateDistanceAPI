756547b1b9402986477e8421700597bc644af0b0
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-11-21 18:06:42 -0300

    test-10

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', async (req, res) => {
 });
 
 async function calculateDistance(origin, destiny) {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
   const apiUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json';
 
   const response = await axios.get(apiUrl, {
